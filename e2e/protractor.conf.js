a44361d06825a07720be04e34befef4025f20f26
Sandra Ortega <sandita.saan@gmail.com>
2019-09-28 11:36:00 +0200

    Rename source folder

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -10,7 +10,7 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './docs/**/*.e2e-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
