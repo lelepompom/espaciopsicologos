83db31c88f64d2903c7e823f20d97d7ce983b464
Sandra Ortega <sandrita.saan@gmail.com>
2019-10-07 02:13:27 +0200

    Team page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './feature/home/home.component';
+import { TeamComponent } from './feature/team/team.component';
+import { LaborComponent } from './feature/labor/labor.component';
+import { AreaComponent } from './feature/area/area.component';
+import { CollaborationComponent } from './feature/collaboration/collaboration.component';
+import { ContactComponent } from './feature/contact/contact.component';
+import { NotfoundComponent } from './core/templates/notfound/notfound.component';
 import { MaintenanceComponent } from './core/templates/maintenance/maintenance.component';
 
 
 const routes: Routes = [
-  { path: '', component: MaintenanceComponent },
-  { path: '**', component: MaintenanceComponent }
+  /**
+   * Production Routes
+   */
+  { path: '', component: HomeComponent },
+  { path: 'equipo', component: TeamComponent },
+  { path: 'trabajo', component: LaborComponent },
+  { path: 'areas', component: AreaComponent },
+  { path: 'colaboraciones', component: CollaborationComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: '**', component: NotfoundComponent }
+
+  /**
+   * Maintenance Routes
+   * { path: '', component: MaintenanceComponent },
+   * { path: '**', component: MaintenanceComponent },
+   */
 ];
 
 @NgModule({
--- src/app/feature/team/team.component.ts
+++ src/app/feature/team/team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-team',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  public assets = '../../../assets/';
+  public inmaculadaDataIndex = 0;
+  public mariaDataIndex = 0;
+
+  constructor(
+    public translate: TranslateService
+  ) { }
 
   ngOnInit() {
   }
 
+  changeData(member: string, action: string): void {
+    switch (action) {
+      case 'prev':
+        if (member === 'inmaculada') {
+          this.inmaculadaDataIndex = this.inmaculadaDataIndex > 0 ? this.inmaculadaDataIndex - 1 : 0;
+        } else {
+          this.mariaDataIndex = this.mariaDataIndex > 0 ? this.mariaDataIndex - 1 : 0;
+        }
+        break;
+      case 'next':
+        this.translate.get('team.members').subscribe(members => {
+          if (member === 'inmaculada') {
+            this.inmaculadaDataIndex = this.inmaculadaDataIndex < members[member].data.length - 1 ?
+              this.inmaculadaDataIndex + 1 :
+              members[member].data.length - 1;
+          } else {
+            this.mariaDataIndex = this.mariaDataIndex < members[member].data.length - 1 ?
+              this.mariaDataIndex + 1 :
+              members[member].data.length - 1;
+          }
+        });
+        break;
+    }
+  }
 }
