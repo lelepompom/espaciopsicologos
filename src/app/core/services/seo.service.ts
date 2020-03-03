2d3ca94e8f9619979aaaafcb8e58f7bc238ecef0
Sandra Ortega <sandrita.saan@gmail.com>
2020-03-03 01:26:52 +0100

    Meta tags

--- src/app/core/services/seo.service.ts
+++ src/app/core/services/seo.service.ts
@@ -14,7 +14,11 @@ export class SeoService {
       this.title.setTitle(section.title);
       this.meta.addTags([
         {name: 'viewport', content: section.viewport},
-        {name: 'author', content: section.author}
+        {name: 'author', content: section.author},
+        {property: 'og:site_name', content: section.title},
+        {property: 'og:title', content: section.title},
+        {property: 'og:description', content: section.description},
+        {property: 'og:url', content: section.url},
       ]);
     });
   }
@@ -23,6 +27,7 @@ export class SeoService {
     this.translate.get(`${page}`).subscribe(section => {
       this.title.setTitle(section.title);
       this.meta.updateTag({name: 'description', content: section.description});
+      this.meta.updateTag({property: 'og:url', content: section.url});
     });
   }
 }
